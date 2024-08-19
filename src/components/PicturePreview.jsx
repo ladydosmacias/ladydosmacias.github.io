02fa42bfd60b0ebd8fd7f9a6c7bfd7260a5124aa
Jasmin Partanen <jasmin.partanen@outlook.com>
2024-08-19 23:46:33 +0300

    resize

--- src/components/PicturePreview.jsx
+++ src/components/PicturePreview.jsx
@@ -26,7 +26,7 @@ const PicturePreview = ({ visible, onClose, image }) => {
     >
       <img
         src={image}
-        style={{ width: windowWidth / 2.8, height: windowHeight / 1.2 }}
+        style={{ height: windowHeight / 1.2 }}
         alt="Logo"
         className="self-center flex mt-7 rounded-lg"
       />
