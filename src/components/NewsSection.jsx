57e08502b6d5ab089e50cd156af6b632d4de0f9a
Jasmin Partanen <jasmin.partanen@outlook.com>
2024-08-25 18:36:07 +0300

    new section update

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -2,8 +2,8 @@ const NewsSection = () => {
   return (
     <div className="p-1 justify-self-stretch bg-red-500 text-center">
       <p>
-        Price list and class content update coming on 25.8.2024! All private
-        dance classes -25% until 31.8.2024, including passes!
+        Class content update coming by the end of 25.8.2024! All private dance
+        classes -25% until 31.8.2024, including passes!
       </p>
     </div>
   );
